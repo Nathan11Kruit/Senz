65d557a9ad13dd78b32fed7e990eff7d3d89c3e9
GoldenNathan <nathan@users.noreply.github.com>
2018-03-19 11:52:16 +0100

    up

--- index.php
+++ index.php
@@ -5,20 +5,79 @@
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <body>
+      <div class="title-block"><p class="title">Dashboard</p></div>
+
   <div class="container">
-    <div class="row">
-    <div class="col-sm-12">
-      <div class="block"><p class="title">Dashboard</p></div>
+  <div class="row">
+    <div class="col-12">
+    <div class="col-12 calendar">
+      <p class="content-title">Maart 2018</p>
     </div>
   </div>
+</div>
+<div class="container">
   <div class="row">
 
-    <div class="col-sm-10">
+    <div class="col-12 date">
 
-      <div class="calender"></div>
+    <div class="row">
+      <div class="col-1"><p class="">1</p></div>
+      <div class="col-1"><p class="">2</p></div>
+      <div class="col-1"><p class="">3</p></div>
+      <div class="col-1"><p class="">4</p></div>
+      <div class="col-1"><p class="">5</p></div>
+      <div class="col-1"><p class="">6</p></div>
+      <div class="col-1"><p class="">7</p></div>
+      <div class="col-1"><p class="">8</p></div>
+      <div class="col-1"><p class="">9</p></div>
+      <div class="col-1"><p class="">10</p></div>
+
+      <div class="col-1"><p class="">11</p></div>
+      <div class="col-1"><p class="">12</p></div>
+      <div class="col-1"><p class="">13</p></div>
+      <div class="col-1"><p class="">14</p></div>
+      <div class="col-1"><p class="">15</p></div>
+      <div class="col-1"><p class="">16</p></div>
+      <div class="col-1"><p class="">17</p></div>
+      <div class="col-1"><p class="">18</p></div>
+      <div class="col-1"><p class="">19</p></div>
+      <div class="col-1"><p class="">20</p></div>
+
+      <div class="col-1"><p class="">21</p></div>
+      <div class="col-1"><p class="">22</p></div>
+      <div class="col-1"><p class="">23</p></div>
+      <div class="col-1"><p class="">24</p></div>
+      <div class="col-1"><p class="">25</p></div>
+      <div class="col-1"><p class="">26</p></div>
+      <div class="col-1"><p class="">27</p></div>
+      <div class="col-1"><p class="">28</p></div>
+      <div class="col-1"><p class="">29</p></div>
+      <div class="col-1"><p class="">30</p></div>
+
+      <div class="col-1"><p class="">31</p></div>
 
     </div>
+    </div>
+    </div>
   </div>
+
+  <div class="container">
+    <div class="row">
+
+<div class="col-12">
+
+  <div class="row">
+
+      <div class="col-6 block2">  <p class="content-title-2">Maart 2018</p></div>
+
+        <div class="col-6 block2">  <p class="content-title-2">Maart 2018</p></div>
+    </div>
+    <div class="row">
+      <div class="col-6 "></div>
+      <div class="col-6 "></div>
+
+
+
 </div>
 </div>
 </div>

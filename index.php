7ee60864c43402f9e834f8f644601c89c946f8ff
GoldenNathan <nathan@users.noreply.github.com>
2018-03-19 10:45:17 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <head>
-kjahsdjhasdjh
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<div class="block"><p class="title">Dashboard</p></div>
+<div class="calender"></div>
+</body>
 </head>

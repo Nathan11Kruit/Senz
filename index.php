1c5588a646a5baee74af51494de1f20f1422d334
GoldenNathan <nathan@users.noreply.github.com>
2018-03-19 13:02:21 +0100

    up

--- index.php
+++ index.php
@@ -5,74 +5,86 @@
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <body>
-      <div class="title-block"><p class="title">Dashboard</p></div>
+      <div class="main-title-block"><h1 class="title text-center">Dashboard</h1></div>
 
-  <div class="container">
-  <div class="row">
-    <div class="col-12">
-    <div class="col-12 spacing-top">
-      <p class="calendar content-title">Maart 2018</p>
-    </div>
-  </div>
-</div>
 <div class="container">
+  <div class="col-12 spacing-top">
+
   <div class="row">
+    <div class="col-12 "> <h4 class="title-block"> Maart 2018</h4></div>
+</div>
+<div class="col-12 ">
 
-    <div class="col-12 date">
+<div class="row date">
 
-    <div class="row">
-      <div class="col-1"><p class="text-center">1</p></div>
-      <div class="col-1"><p class="text-center">2</p></div>
-      <div class="col-1"><p class="text-center">3</p></div>
-      <div class="col-1"><p class="text-center">4</p></div>
-      <div class="col-1"><p class="text-center">5</p></div>
-      <div class="col-1"><p class="text-center">6</p></div>
-      <div class="col-1"><p class="text-center">7</p></div>
-      <div class="col-1"><p class="text-center">8</p></div>
-      <div class="col-1"><p class="text-center">9</p></div>
-      <div class="col-1"><p class="text-center">10</p></div>
-
-      <div class="col-1"><p class="text-center">11</p></div>
-      <div class="col-1"><p class="text-center">12</p></div>
-      <div class="col-1"><p class="text-center">13</p></div>
-      <div class="col-1"><p class="text-center">14</p></div>
-      <div class="col-1"><p class="text-center">15</p></div>
-      <div class="col-1"><p class="text-center">16</p></div>
-      <div class="col-1"><p class="text-center">17</p></div>
-      <div class="col-1"><p class="text-center">18</p></div>
-      <div class="col-1"><p class="text-center">19</p></div>
-      <div class="col-1"><p class="text-center">20</p></div>
-
-      <div class="col-1"><p class="text-center">21</p></div>
-      <div class="col-1"><p class="text-center">22</p></div>
-      <div class="col-1"><p class="text-center">23</p></div>
-      <div class="col-1"><p class="text-center">24</p></div>
-      <div class="col-1"><p class="text-center">25</p></div>
-      <div class="col-1"><p class="text-center">26</p></div>
-      <div class="col-1"><p class="text-center">27</p></div>
-      <div class="col-1"><p class="text-center">28</p></div>
-      <div class="col-1"><p class="text-center">29</p></div>
-      <div class="col-1"><p class="text-center">30</p></div>
-
-      <div class="col-1"><p class="text-center">31</p></div>
-
-    </div>
-    </div>
-    </div>
-  </div>
-
-  <div class="container spacing-top">
-    <div class="row">
+<div class="col-1"><p class="text-center">1</p></div>
+<div class="col-1"><p class="text-center">2</p></div>
+<div class="col-1"><p class="text-center">3</p></div>
+<div class="col-1"><p class="text-center">4</p></div>
+<div class="col-1"><p class="text-center">5</p></div>
+<div class="col-1"><p class="text-center">6</p></div>
+<div class="col-1"><p class="text-center">7</p></div>
+<div class="col-1"><p class="text-center">8</p></div>
+<div class="col-1"><p class="text-center">9</p></div>
+<div class="col-1"><p class="text-center">10</p></div>
+<div class="col-1"><p class="text-center">11</p></div>
+<div class="col-1"><p class="text-center">12</p></div>
+<div class="col-1"><p class="text-center">13</p></div>
+<div class="col-1"><p class="text-center">14</p></div>
+<div class="col-1"><p class="text-center">15</p></div>
+<div class="col-1"><p class="text-center">16</p></div>
+<div class="col-1"><p class="text-center">17</p></div>
+<div class="col-1"><p class="text-center">18</p></div>
+<div class="col-1"><p class="text-center">19</p></div>
+<div class="col-1"><p class="text-center">20</p></div>
+<div class="col-1"><p class="text-center">21</p></div>
+<div class="col-1"><p class="text-center">22</p></div>
+<div class="col-1"><p class="text-center">23</p></div>
+<div class="col-1"><p class="text-center">24</p></div>
+<div class="col-1"><p class="text-center">25</p></div>
+<div class="col-1"><p class="text-center">26</p></div>
+<div class="col-1"><p class="text-center">27</p></div>
+<div class="col-1"><p class="text-center">28</p></div>
+<div class="col-1"><p class="text-center">29</p></div>
+<div class="col-1"><p class="text-center">30</p></div>
+</div>
+</div>
 
 
+<div class="row spacing-top">
+  <div class="col-6">  <h6 class="title-block text-center">Hoe te scheiden?</h6></div>
+  <div class="col-6">  <h6 class="title-block text-center">Meld een probleem</h6></div>
+</div>
+<div class="row">
+  <div class="col-6"> <div class="small-block"></div> </div>
+  <div class="col-6">  <div class="small-block"></div></div>
+</div>
+<div class="row spacing-top">
+  <div class="col-6">  <h6 class="title-block text-center">Mijn donaties</h6></div>
+  <div class="col-6">  <h6 class="title-block text-center">Locatie container</h6></div>
+</div>
+<div class="row">
+  <div class="col-6"> <div class="small-block"></div> </div>
+  <div class="col-6">  <div class="small-block"></div></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
-      <div class="col-6">  <p class="block-2 content-title-2">Maart 2018</p></div>
 
-        <div class="col-6">  <p class="block-2 content-title-2">Maart 2018</p></div>
-    </div>
-    <div class="row">
-      <div class="col-6 "></div>
-      <div class="col-6 "></div>
+<footer>
+<div class="container-fluid">
+
+<div class="row menu-1">
+
+  <div class="col-4 menu"></div>
+  <div class="col-4 menu"></div>
+  <div class="col-4 menu"></div>
+
+
+
 
 
 
@@ -80,6 +92,7 @@
 </div>
 </div>
 </div>
+
 </div>
 </div>
 </body>

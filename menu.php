35c352eed2af7406196341fccfb47b12e29de535
Bernt Muurling <w.b.h.muurling@st.hanze.nl>
2018-03-19 13:44:26 +0100

    Menu

--- menu.php
+++ menu.php
@@ -1,11 +1,9 @@
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <footer>
 
 <div class="container-fluid">
 	<div class="row menu-1">
-	  <div class="col-4 menu"></div>
-	  <div class="col-4 menu"></div>
-	  <div class="col-4 menu"></div>
+	  <a href=""><div class="col-4 menu-block">Dashboard</div></a>
+	  <a href=""><div class="col-4 menu-block">Container</div></a>
+	  <a href=""><div class="col-4 menu-block">Instellingen</div></a>
 	</div>
 </div>

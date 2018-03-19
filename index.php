7b28c33a89d5a28e4b8ea9d1efb870996c1107e2
GoldenNathan <nathan@users.noreply.github.com>
2018-03-19 13:33:38 +0100

    up

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
 <body>
       <div class="main-title-block"><h1 class="title text-center">Dashboard</h1></div>
 

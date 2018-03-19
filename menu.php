6590183bf85a21b6cc8533b1aafdc66e0bedad7a
Bernt Muurling <w.b.h.muurling@st.hanze.nl>
2018-03-19 14:14:57 +0100

    UPLOAD

--- menu.php
+++ menu.php
@@ -2,8 +2,8 @@
 
 <div class="container-fluid">
 	<div class="row menu-1">
-	  <a href=""><div class="col-4 menu-block">Dashboard</div></a>
-	  <a href=""><div class="col-4 menu-block">Container</div></a>
-	  <a href=""><div class="col-4 menu-block">Instellingen</div></a>
+	  <a href="" class="col-4 menu-block1"><img class="center-image" height="50px" src="images/noun_293_cc.svg">Dashboard</a>
+	  <a href="" class="col-4 menu-block1">Container</a>
+	  <a href="" class="col-4 menu-block1">Instellingen</a>
 	</div>
 </div>

7c51afdd1c93e0e1b5b304c19d86e3920ca7f002
GoldenNathan <nathan@users.noreply.github.com>
2018-03-19 12:06:20 +0100

    up

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
   <div class="container">
   <div class="row">
     <div class="col-12">
-    <div class="col-12 calendar">
-      <p class="content-title">Maart 2018</p>
+    <div class="col-12 spacing-top">
+      <p class="calendar content-title">Maart 2018</p>
     </div>
   </div>
 </div>
@@ -21,56 +21,54 @@
     <div class="col-12 date">
 
     <div class="row">
-      <div class="col-1"><p class="">1</p></div>
-      <div class="col-1"><p class="">2</p></div>
-      <div class="col-1"><p class="">3</p></div>
-      <div class="col-1"><p class="">4</p></div>
-      <div class="col-1"><p class="">5</p></div>
-      <div class="col-1"><p class="">6</p></div>
-      <div class="col-1"><p class="">7</p></div>
-      <div class="col-1"><p class="">8</p></div>
-      <div class="col-1"><p class="">9</p></div>
-      <div class="col-1"><p class="">10</p></div>
+      <div class="col-1"><p class="text-center">1</p></div>
+      <div class="col-1"><p class="text-center">2</p></div>
+      <div class="col-1"><p class="text-center">3</p></div>
+      <div class="col-1"><p class="text-center">4</p></div>
+      <div class="col-1"><p class="text-center">5</p></div>
+      <div class="col-1"><p class="text-center">6</p></div>
+      <div class="col-1"><p class="text-center">7</p></div>
+      <div class="col-1"><p class="text-center">8</p></div>
+      <div class="col-1"><p class="text-center">9</p></div>
+      <div class="col-1"><p class="text-center">10</p></div>
 
-      <div class="col-1"><p class="">11</p></div>
-      <div class="col-1"><p class="">12</p></div>
-      <div class="col-1"><p class="">13</p></div>
-      <div class="col-1"><p class="">14</p></div>
-      <div class="col-1"><p class="">15</p></div>
-      <div class="col-1"><p class="">16</p></div>
-      <div class="col-1"><p class="">17</p></div>
-      <div class="col-1"><p class="">18</p></div>
-      <div class="col-1"><p class="">19</p></div>
-      <div class="col-1"><p class="">20</p></div>
+      <div class="col-1"><p class="text-center">11</p></div>
+      <div class="col-1"><p class="text-center">12</p></div>
+      <div class="col-1"><p class="text-center">13</p></div>
+      <div class="col-1"><p class="text-center">14</p></div>
+      <div class="col-1"><p class="text-center">15</p></div>
+      <div class="col-1"><p class="text-center">16</p></div>
+      <div class="col-1"><p class="text-center">17</p></div>
+      <div class="col-1"><p class="text-center">18</p></div>
+      <div class="col-1"><p class="text-center">19</p></div>
+      <div class="col-1"><p class="text-center">20</p></div>
 
-      <div class="col-1"><p class="">21</p></div>
-      <div class="col-1"><p class="">22</p></div>
-      <div class="col-1"><p class="">23</p></div>
-      <div class="col-1"><p class="">24</p></div>
-      <div class="col-1"><p class="">25</p></div>
-      <div class="col-1"><p class="">26</p></div>
-      <div class="col-1"><p class="">27</p></div>
-      <div class="col-1"><p class="">28</p></div>
-      <div class="col-1"><p class="">29</p></div>
-      <div class="col-1"><p class="">30</p></div>
+      <div class="col-1"><p class="text-center">21</p></div>
+      <div class="col-1"><p class="text-center">22</p></div>
+      <div class="col-1"><p class="text-center">23</p></div>
+      <div class="col-1"><p class="text-center">24</p></div>
+      <div class="col-1"><p class="text-center">25</p></div>
+      <div class="col-1"><p class="text-center">26</p></div>
+      <div class="col-1"><p class="text-center">27</p></div>
+      <div class="col-1"><p class="text-center">28</p></div>
+      <div class="col-1"><p class="text-center">29</p></div>
+      <div class="col-1"><p class="text-center">30</p></div>
 
-      <div class="col-1"><p class="">31</p></div>
+      <div class="col-1"><p class="text-center">31</p></div>
 
     </div>
     </div>
     </div>
   </div>
 
-  <div class="container">
+  <div class="container spacing-top">
     <div class="row">
 
-<div class="col-12">
 
-  <div class="row">
 
-      <div class="col-6 block2">  <p class="content-title-2">Maart 2018</p></div>
+      <div class="col-6">  <p class="block-2 content-title-2">Maart 2018</p></div>
 
-        <div class="col-6 block2">  <p class="content-title-2">Maart 2018</p></div>
+        <div class="col-6">  <p class="block-2 content-title-2">Maart 2018</p></div>
     </div>
     <div class="row">
       <div class="col-6 "></div>

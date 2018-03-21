6e97b93864011bce3fd4138345a24d5238456c3e
Bernt Muurling <w.b.h.muurling@st.hanze.nl>
2018-03-21 14:48:59 +0100

    UPDATERINO 3.4

--- menu.php
+++ menu.php
@@ -2,8 +2,8 @@
 
 <div class="container-fluid">
 	<div class="row menu-1">
-	  <a href="" class="col-4 menu-block1"><img class="center-image" height="35px" src="images/dashboard.svg">Dashboard</a>
-	  <a href="" class="col-4 menu-block1"><img class="center-image" height="35px" src="images/container.svg">Container</a>
-	  <a href="" class="col-4 menu-block1"><img class="center-image" height="35px" src="images/settings.svg">Instellingen</a>
+	  <a href="" class="col-4 menu-block1"><img class="center-image" height="30px" src="images/dashboard.svg">Dashboard</a>
+	  <a href="" class="col-4 menu-block1"><img class="center-image" height="30px" src="images/container.svg">Container</a>
+	  <a href="" class="col-4 menu-block1"><img class="center-image" height="30px" src="images/settings.svg">Instellingen</a>
 	</div>
 </div> 

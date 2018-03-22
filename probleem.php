4e4c9a6d6cedccb17fd37228317f57c4855dadcc
Bernt Muurling <w.b.h.muurling@st.hanze.nl>
2018-03-22 13:18:53 +0100

    PROBLEEMN POAR NEMN

--- probleem.php
+++ probleem.php
@@ -4,24 +4,22 @@
       <div class="main-title-block"><h1 class="title text-center">Probleem melden</h1></div>
 
   <div class="container">
-    <div class="col-12 spacing-top">
-      <div class="col-12 ">
-        <div class="row date">
-  <p>Help ons verbeteren door problemen tijdig door te geven. Is een container onverhoopt vol geraakt of loopt u tegen een ander probleem aan? Meld het ons.</p>
-      <form action="/action_page.php">
-      <input type="text" name="naam" placeholder="Uw naam">
-      <input type="email" name="email" placeholder="Uw e-mailadres">
-      <input type="text" name="conainternummer" placeholder="Containernummer">
-      <input type="radio" name="probleem1" value="1">
-      <input type="textfield" name="bericht" placeholder="Licht uw antwoord toe">
-
-    </form>
-
-
-    <div class="col-md-4 text-center">  
-      <button class="btn btn-primary knoptekst">Verzenden</button>
-    </div>
-        </div>
+    <div class="row spacing-top probleemcontainer">
+      <div class="col-12 date">
+          <p class="probleemtext">Help ons verbeteren door problemen tijdig door te geven. Is een container onverhoopt vol geraakt of loopt u tegen een ander probleem aan? Meld het ons.</p>
+              <form action="">
+              <input class="probleemform" type="text" name="naam" placeholder="Uw naam"><br>
+              <input class="probleemform" type="email" name="email" placeholder="Uw e-mailadres"><br>
+              <input class="probleemform" type="number" name="conainternummer" placeholder="Containernummer"><br>
+                <input class="probleemradio" type="radio" name="probleem1" value="1"> Container zit vol <br>
+                <input class="probleemradio" type="radio" name="probleem1" value="2"> Container kan niet sluiten/openen <br>
+                <input class="probleemradio" type="radio" name="probleem1" value="3"> Ligt afval naast de container <br>
+                <input class="probleemradio" type="radio" name="probleem1" value="4" checked> Anders: <br>
+              <textarea class="probleemarea" rows="4" placeholder="Licht uw antwoord toe"></textarea>      
+              <div class="col-md-4 text-center">  
+                <input type="submit" class="koptekst probleemsubmit" value="Verzenden">
+              </div>
+            </form>
       </div>
     </div>
   </div>

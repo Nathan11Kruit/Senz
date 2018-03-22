ed4a41514c7df3b34af00e6202b416a2f1719cb0
Thomas <t.e.hakvoort@st.hanze.nl>
2018-03-22 13:47:51 +0100

    lol
    lynn

---
+++ meldingenpaginabedrijf.php
@@ -0,0 +1,121 @@
+<?php include 'header.php'; ?>
+
+<body>
+
+      <div class="main-title-block"><h1 class="title text-center">Meldingen</h1></div>
+
+<div class="container">
+
+  <div class="col-12 spacing-top">
+	<div class="row">
+		<div class="col-8">
+		<div class="searchbar">
+  			<input type="text" placeholder="Zoeken" class="form-control">
+		</div>
+	</div>
+	<div class="col-4">
+		<button type="button" class="btn btn-primary btn-sm sorteren">Sorteren</button>
+	</div>
+</div>
+</div>
+
+<div class="achtergrond-instellingen">	   
+
+	<div class="container">
+  		<div class="col-12 spacing-top">
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-8">
+			    			<p class="containernaam">Container</p>
+			    			<p class="containerlocatie">Egginklaan, Utrecht</p>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-8">
+			    			<p class="containernaam">Container</p>
+			    			<p class="containerlocatie">Egginklaan, Utrecht</p>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-8">
+			    			<p class="containernaam">Container</p>
+			    			<p class="containerlocatie">Egginklaan, Utrecht</p>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-8">
+			    			<p class="containernaam">Container</p>
+			    			<p class="containerlocatie">Egginklaan, Utrecht</p>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-8">
+			    			<p class="containernaam">Container</p>
+			    			<p class="containerlocatie">Egginklaan, Utrecht</p>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-8">
+			    			<p class="containernaam">Container</p>
+			    			<p class="containerlocatie">Egginklaan, Utrecht</p>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+
+	</div>
+  </div>
+ </div>
+
+
+
+
+
+</div>
+
+
+
+<?php include 'menu.php'; ?>
+
+<?php include 'footer.php'; ?>
+
+
+
+
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</body>
+

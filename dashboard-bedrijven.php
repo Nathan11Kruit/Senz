71373ebe6610fc09a806ec1a9d562fdfcbade6f4
GoldenNathan <nathan@users.noreply.github.com>
2018-03-26 14:08:24 +0200

    kasdgksdgk

---
+++ dashboard-bedrijven.php
@@ -0,0 +1,69 @@
+<?php
+  include 'header.php';
+?>
+<body>
+      <div class="main-title-block"><h1 class="title text-center">Dashboard</h1></div>
+
+<div class="container">
+
+  <div class="row spacing-top">
+    <div class="col-12 "><h6 class="title-block calender-title"> Overzicht containers</h6></div>
+    <div class="col-12 ">
+      <div class="text-block">
+      <p class="no-margin">
+      <strong>Container 221</br>
+        Luchtvochtigheid boven de 70%</strong></p>
+        <p class="timestamp"> 14 maart, 7:30</p>
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="row spacing-top">
+  <div class="col-12 "><h6 class="title-block calender-title"> Meldingen van gebruikers</h6></div>
+  <div class="col-12 ">
+    <div class="text-block">
+    <p class="no-margin">
+      <strong>Container melding</strong></br>
+      <p class="timestamp no-margin">Peltlaan, Utrecht, 14 maart, 7:30 dit is te lang</p>
+      <p class="melding"> Ik ben een gebruiker en ik heb iets waar ik pissed over ben g.</p>
+  </div>
+</div>
+</div>
+
+<div class="row spacing-top">
+    <div class="col-12 "> <h6 class="title-block">Locatie</h6></div>
+</div>
+<div class="col-12 ">
+<div class="row date">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1271.8946052697713!2d6.55601429066752!3d53.222179209723826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c9cd45eb689603%3A0x382349948753999a!2sGrote+Rozenstraat%2C+Groningen!5e0!3m2!1snl!2snl!4v1521642485184" width="600" height="175" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+</div>
+</div>
+
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<?php include 'menu-bedrijven.php'; ?>
+
+
+
+
+
+
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+<?php
+  include 'footer.php';
+?>
---
+++ instellingen-bedrijven.php
@@ -0,0 +1,67 @@
+<?php
+	include 'header.php';
+?>
+
+<body>
+	<div class="achtergrond-instellingen">
+	      <div class="main-title-block">
+					<a href="dashboard-bedrijven.php"><img class="center-image back" src="images/whitearrow.png"></a><div class="main-title-block"><h1 class="title text-center">Instellingen</h1></div>
+	      </div>
+
+	<div class="container">
+  		<div class="col-12 spacing-top">
+		  	<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-9">
+			    			<p class="balkinhoud">Push notificaties</p>
+			    		</div>
+			    		<div class="col-1">
+			    			<div class="balkinhoud">
+			    				<label class="switch">
+								  <input type="checkbox">
+								  <span class="slider round"></span>
+								</label>
+			    			</div>
+			    		</div>
+			    	</div>
+			    </div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<div class="row">
+			    		<div class="col-9">
+			    			<p class="balkinhoud">Live locatie</p>
+			    		</div>
+			    		<div class="col-1">
+			    			<div class="balkinhoud">
+			    				<label class="switch">
+								  <input type="checkbox">
+								  <span class="slider round"></span>
+								</label>
+			    			</div>
+			    		</div>
+			    	</div>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<p class="balkinhoud center">Voorwaarden en privacy</p>
+		    	</div>
+			</div>
+
+			<div class="row">
+		    	<div class="col-12 balkinstellingen">
+		    		<p class="balkinhoud center">Uitloggen</p>
+		    	</div>
+			</div>
+		</div>
+	</div>
+
+
+<?php
+	include 'footer.php';
+	include 'menu-bedrijven.php';
+?>

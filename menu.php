2a5c04bec427833382c4e738446afd2be84e0557
GoldenNathan <nathan@users.noreply.github.com>
2018-03-26 15:33:32 +0200

    gdsfgdf

--- menu.php
+++ menu.php
@@ -1,7 +1,10 @@
+<div style="height:70px;"></div>
+
+
 <footer>
 <nav>
 <div class="container-fluid">
-	<div class="row menu-2">
+	<div class="row menu-1">
 	  <a href="index.php" class="col-4 menu-block1"><img class="center-image" height="30px" src="images/dashboard.svg">Dashboard</a>
 	  <a href="containerinfo.php" class="col-4 menu-block1"><img class="center-image" height="30px" src="images/container.svg">Container</a>
 	  <a href="instellingen.php" class="col-4 menu-block1"><img class="center-image" height="30px" src="images/settings.svg">Instellingen</a>
